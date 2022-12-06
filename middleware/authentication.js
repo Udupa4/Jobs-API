d5f029adbb10e7ff6d30f0cb17c17ecda0917610
Udupa <nishuclan@gmail.com>
2022-12-07 00:04:30 +0530

    semi-colon added to all files

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,26 +1,26 @@
 // middleware to verify the token
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-const {UnauthenticatedError} = require('../errors')
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
 
 // middleware
 const auth = async (req, res, next) => {
-    // check headers
-    const authHeader = req.headers.authorization
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new UnauthenticatedError('Authentication invalid')
-    }
+  // check headers
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
 
-    const token = authHeader.split(' ')[1]
+  const token = authHeader.split(" ")[1];
 
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-        // attach the user to the job routes
-        req.user = {userID: payload.userID, name: payload.name}
-        next()
-    } catch (error) {
-        throw new UnauthenticatedError('Authentication failed')
-    }
-}
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach the user to the job routes
+    req.user = { userID: payload.userID, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication failed");
+  }
+};
 
-module.exports = auth
+module.exports = auth;
--- models/Job.js
+++ models/Job.js
@@ -1,26 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const JobSchema = new mongoose.Schema({
+const JobSchema = new mongoose.Schema(
+  {
     company: {
-        type: String,
-        required: [true, 'Please provide company name'],
-        maxlenght: 50
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlenght: 50,
     },
     position: {
-        type: String,
-        required: [true, 'Please provide a position'],
-        maxlength: 100
+      type: String,
+      required: [true, "Please provide a position"],
+      maxlength: 100,
     },
     status: {
-        type: String,
-        enum: ['interview', 'declined', 'pending'],
-        default: 'pending'
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
     },
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide user']
-    }
-}, {timestamps: true})
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Job', JobSchema)
+module.exports = mongoose.model("Job", JobSchema);
