0f06f4a26f047b3abc8e531e08979ff629d7070a
Udupa <nishuclan@gmail.com>
2022-12-07 00:01:31 +0530

    semi-colon added to routes/jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {getAlljobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob
-} = require('../controllers/jobs')
+const {
+  getAlljobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobs");
 
-router.route('/').post(createJob).get(getAlljobs)
-router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
+router.route("/").post(createJob).get(getAlljobs);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
-module.exports = router
+module.exports = router;
